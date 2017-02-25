06baee690a0815f46f25a3c3258e5e3a8d054607
Connor Walsh <connorwalsh888@hotmail.com>
2017-02-25 15:54:03 -0400

    Finishing advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,6 +21,7 @@ module SessionsHelper
   end
 
   def log_out
+    forget(@current_user)
     session.delete(:user_id)
     @current_user = nil
   end
@@ -28,7 +29,14 @@ module SessionsHelper
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent.signed[:remember_token] = user.remember_token
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+
   end
 
 end
